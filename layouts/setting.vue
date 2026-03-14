007956e0435d332668029162d10b4c5d81974d7d
Vizxyyy <rafaelababa9@gmail.com>
2026-03-14 21:56:23 +0800

    layout for settings

---
+++ layouts/setting.vue
@@ -0,0 +1,20 @@
+<template>
+
+<div>
+
+<h1 class="text-2xl font-bold border-b p-6">
+Settings
+</h1>
+
+<div class="flex gap-6 border-b px-6 py-3">
+  <NuxtLink to="/setting/general">General</NuxtLink>
+  <NuxtLink to="/setting/security">Security</NuxtLink>
+</div>
+
+<main class="p-6">
+  <slot />
+</main>
+
+</div>
+
+</template>
