1032d8d69313a845b4bfea71b689a23022d79298
fk-Prince <sestoso.prince@gmail.com>
2026-03-15 01:17:09 +0800

    z

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,9 @@
+import { NuxtLink } from '../.nuxt/components';
 
 <template>
     Landing Page
+
+   <NuxtLink to="/auth/signin">
+      <button class="p-5 bg-green-600 text-white rounded-sm">LOGIN</button>
+    </NuxtLink>
 </template>
