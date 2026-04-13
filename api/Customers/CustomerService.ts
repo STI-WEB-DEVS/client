2df0c0c4689f294d7d32053265205bd797c6e856
FlareItsh <rayverdasalla@gmail.com>
2026-04-13 14:07:52 +0800

    feat: replace Team module with Customer service and page, and reformat layout component

---
+++ api/Customers/CustomerService.ts
@@ -0,0 +1,11 @@
+import { BaseService } from "../BaseService";
+
+export class CustomerService extends BaseService {
+  async getCustomers() {
+    return this.request("/customers", "GET");
+  }
+
+  async createCustomer(customerData: object) {
+    return this.request("/customers", "POST", customerData);
+  }
+}
