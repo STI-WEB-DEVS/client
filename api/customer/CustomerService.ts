ad4dbd3378c7573cdf204369d6e270fa6e99708b
Keith3123 <nicolemoralesberou35@gmail.com>
2026-04-25 16:42:57 +0800

    commit

--- api/customer/CustomerService.ts
+++ api/customer/CustomerService.ts
@@ -1,40 +1,12 @@
-import BaseService from '~/api/BaseService';
+import { BaseService } from "../BaseService"; 
 
-class CustomerService extends BaseService {
-    private static instance: CustomerService;
+export class CustomerService extends BaseService {
+  async getCustomers() {
+    return this.request('/customers', 'GET');
+  }
 
-    public static getInstance(): CustomerService {
-        if (!CustomerService.instance) {
-            CustomerService.instance = new CustomerService();
-        }
-        return CustomerService.instance;
-    }
-
-    private resource = '/customers';
-
-    async list(params: object = {}): Promise<any> {
-        return await this.request(this.resource, 'GET', params);
-    }
-
-    async create(payload: object): Promise<any> {
-        return await this.request(this.resource, 'POST', payload);
-    }
-
-    async show(uuid: string): Promise<any> {
-        return await this.request(`${this.resource}/${uuid}`, 'GET');
-    }
-
-    async update(uuid: string, payload: object): Promise<any> {
-        return await this.request(`${this.resource}/${uuid}`, 'PUT', payload);
-    }
-
-    async delete(uuid: string): Promise<any> {
-        return await this.request(`${this.resource}/${uuid}`, 'DELETE');
-    }
-
-    async restore(uuid: string): Promise<any> {
-        return await this.request(`${this.resource}/${uuid}/restore`, 'POST');
-    }
+  async createCustomer(customerData: object) {
+    return this.request('/customers', 'POST', customerData);
+  }
+  
 }
-
-export const customerService = CustomerService.getInstance();
