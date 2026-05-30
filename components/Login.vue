57502b06e6c0a91fd128574e9f228999d430f46f
joshhyung <joshuagalo137@gmail.com>
2026-05-30 12:08:43 +0800

    unfinish

--- components/Login.vue
+++ components/Login.vue
@@ -70,12 +70,50 @@
 </template>
 
 <script setup lang="ts">
-/**
- * HandleSubmit function para sa navigation redirection.
- * Gagamit tayo ng navigateTo (Nuxt utility) para sigurado ang pagpunta sa dashboard.
- */
+definePageMeta({
+  layout: false,
+});
+ 
+import { ref } from "vue";
+import { AuthService } from "~/api/auth/AuthService";
+ 
+const email = ref("");
+const password = ref("");
+const error = ref("");
+const isLoading = ref(false);
+ 
+const authService = new AuthService();
+ 
 const handleSubmit = async () => {
-  // Simple simulation of login success
-  await navigateTo('/dashboard')
-}
+  error.value = "";
+  isLoading.value = true;
+ 
+  try {
+    const response = await authService.login(email.value, password.value);
+ 
+    if (response?.token) {
+      localStorage.setItem("_token", response.token);
+    }
+ 
+    // // if (response?.user.customer_uuid) {
+    // //   localStorage.setItem("_uuid", response.user.customer_uuid);
+    // // } else if (response?.user.uuid) {
+    // //   localStorage.setItem("_uuid", response.user.uuid);
+    // // }
+ 
+    / /// if (response?.user.role) {
+    // //   localStorage.setItem("_role", response.user.role);
+    // // }
+ 
+    // await navigateTo("/admin/dashboard");
+ 
+    await navigateTo(
+      response.user.role === "admin" ? "/admin/dashboard" : "/dashboard",
+    );
+  } catch (err: any) {
+    error.value = err?.message || "";
+  } finally {
+    isLoading.value = false;
+  }
+};
 </script>
